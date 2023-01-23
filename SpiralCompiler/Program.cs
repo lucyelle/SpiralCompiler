d52086b90f40d14b6283579d7aac6623f9d65658
lucyelle <lucyelle@users.noreply.github.com>
2023-01-23 16:30:24 +0100

    Initial commit

---
+++ SpiralCompiler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SpiralCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
