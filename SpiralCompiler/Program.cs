30be180a8b159f51efd9cd50cd62023f324fab8c
lucyelle <lucyelle@users.noreply.github.com>
2023-01-23 17:58:45 +0100

    Lexer base

--- SpiralCompiler/Program.cs
+++ SpiralCompiler/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace SpiralCompiler
+﻿using SpiralCompiler.Syntax;
+
+namespace SpiralCompiler
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var tokens = Lexer.Lex(File.ReadAllText("testcode_tokens.txt"));
+
+            var output = new List<string>();
+            foreach (var token in tokens) {
+                output.Add(token.Text);
+            }
+            File.WriteAllLines("testcode_out.txt", output);
         }
     }
 }
