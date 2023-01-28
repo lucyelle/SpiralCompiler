df4231d7699a0dfc430fc0d3537e8cd7596d9ad0
lucyelle <lucyelle@users.noreply.github.com>
2023-01-28 18:18:20 +0100

    Fixes based on review

--- SpiralCompiler/Program.cs
+++ SpiralCompiler/Program.cs
@@ -1,19 +1,23 @@
 ﻿using SpiralCompiler.Syntax;
 
-namespace SpiralCompiler
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var tokens = Lexer.Lex(File.ReadAllText("testcode_tokens.txt"));
+namespace SpiralCompiler;
 
-            using var output = new FileStream("tokens_out.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            using var writer = new StreamWriter(output);
-            foreach (var t in tokens)
-            {
-                writer.WriteLine($"'{t.Text}' => {t.Type} {t.Position.Start}-{t.Position.End}");
-            }
+public class Program
+{
+    static void Main(string[] args)
+    {
+        TokensTest("testcode_tokens.txt", "tokens_out.txt");
+    }
+
+    private static void TokensTest(string inputPath, string outputPath)
+    {
+        var tokens = Lexer.Lex(File.ReadAllText(inputPath));
+
+        using var output = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write);
+        using var writer = new StreamWriter(output);
+        foreach (var t in tokens)
+        {
+            writer.WriteLine($"'{t.Text}' => {t.Type} {t.Position.Start}-{t.Position.End}");
         }
     }
 }
