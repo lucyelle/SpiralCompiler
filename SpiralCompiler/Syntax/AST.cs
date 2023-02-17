1346ef5a7bf49608855614ffeef56ec55e831506
lucyelle <lucyelle@users.noreply.github.com>
2023-02-17 13:12:14 +0100

    Updated grammar

---
+++ SpiralCompiler/Syntax/AST.cs
@@ -0,0 +1,51 @@
+﻿namespace SpiralCompiler.Syntax
+{
+    public class AST
+    {
+    }
+
+    public abstract record class Statement
+    {
+
+    }
+
+    public abstract record class Expression
+    {
+        public record class Integer(int Value) : Expression;
+        public record class Double(double Value) : Expression;
+        public record class String(string Value) : Expression;
+        public record class Boolean(bool Value) : Expression;
+        public record class Binary(Expression Left, BinOp Op, Expression Right) : Expression;
+        public record class UnaryPre(UnOpPre Op, Expression Right) : Expression;
+        public record class UnaryPost(Expression Left, UnOpPost Op) : Expression;
+        public record class Identifier(string Name) : Expression;
+    }
+
+    public enum BinOp
+    {
+        Assign,
+        Add,
+        Substract,
+        Multiply,
+        Divide,
+        Equals,
+        Less,
+        Greater,
+        LessEquals,
+        GreaterEquals,
+        Or,
+        And
+    }
+
+    public enum UnOpPre
+    {
+        Minus,
+        Not,
+    }
+
+    public enum UnOpPost
+    {
+        Increment,
+        Decrement,
+    }
+}
