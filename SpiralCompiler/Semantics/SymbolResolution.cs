66c9fd6102612d81a25c92c25e5bec18c8b7640c
lucyelle <lucyelle@users.noreply.github.com>
2023-03-07 20:06:37 +0100

    Basic symbol resolution

---
+++ SpiralCompiler/Semantics/SymbolResolution.cs
@@ -0,0 +1,155 @@
+using SpiralCompiler.Syntax;
+
+namespace SpiralCompiler.Semantics;
+public class SymbolResolutionStage1 : AstVisitorBase<Unit>
+{
+    private Scope currentScope;
+    public Scope RootScope { get; }
+
+    public SymbolResolutionStage1()
+    {
+        RootScope = new Scope(null);
+        currentScope = RootScope;
+        RootScope.AddSymbol(new Symbol.Type.Primitive("string", typeof(string)));
+        RootScope.AddSymbol(new Symbol.Type.Primitive("int", typeof(int)));
+    }
+
+    protected override Unit VisitFunctionDefStatement(Statement.FunctionDef node)
+    {
+        var symbol = new Symbol.Function(node.Name);
+        AddSymbol(symbol);
+
+        node.Scope = PushScope();
+        base.VisitFunctionDefStatement(node);
+        PopScope();
+
+        return default;
+    }
+
+    protected override Unit VisitBlockStatement(Statement.Block node)
+    {
+        node.Scope = PushScope();
+        base.VisitBlockStatement(node);
+        PopScope();
+
+        return default;
+    }
+
+    protected override Unit VisitVarStatement(Statement.Var node)
+    {
+        base.VisitVarStatement(node);
+
+        if (currentScope == RootScope)
+        {
+            var symbol = new Symbol.Variable(node.Name);
+            AddSymbol(symbol);
+        }
+
+        return default;
+    }
+
+    protected override Unit VisitParameter(Parameter param)
+    {
+        base.VisitParameter(param);
+
+        var symbol = new Symbol.Variable(param.Name);
+        AddSymbol(symbol);
+
+        return default;
+    }
+
+    private Scope PushScope()
+    {
+        currentScope = new Scope(currentScope);
+        return currentScope;
+    }
+
+    private void PopScope()
+    {
+        if (currentScope.Parent is null)
+        {
+            throw new InvalidOperationException();
+        }
+        currentScope = currentScope.Parent;
+    }
+
+    private void AddSymbol(Symbol symbol)
+    {
+        currentScope.AddSymbol(symbol);
+    }
+}
+
+public class SymbolResolutionStage2 : AstVisitorBase<Unit>
+{
+    private Scope currentScope;
+
+    public SymbolResolutionStage2(Scope root)
+    {
+        currentScope = root;
+    }
+
+    protected override Unit VisitFunctionDefStatement(Statement.FunctionDef node)
+    {
+        PushScope(node.Scope);
+        base.VisitFunctionDefStatement(node);
+        PopScope();
+
+        return default;
+    }
+
+    protected override Unit VisitBlockStatement(Statement.Block node)
+    {
+        PushScope(node.Scope);
+        base.VisitBlockStatement(node);
+        PopScope();
+
+        return default;
+    }
+
+    protected override Unit VisitVarStatement(Statement.Var node)
+    {
+        base.VisitVarStatement(node);
+        if (currentScope.Parent is not null)
+        {
+            var symbol = new Symbol.Variable(node.Name);
+            AddSymbol(symbol);
+        }
+
+        return default;
+    }
+
+    protected override Unit VisitIdentifierExpression(Expression.Identifier node)
+    {
+        var symbol = currentScope.SearchSymbol(node.Name);
+        return default;
+    }
+
+    protected override Unit VisitIdentifierTypeReference(TypeReference.Identifier node)
+    {
+        var symbol = currentScope.SearchSymbol(node.Name);
+        return default;
+    }
+
+    private void PushScope(Scope? scope)
+    {
+        if (scope is null)
+        {
+            throw new ArgumentNullException(nameof(scope));
+        }
+        currentScope = scope;
+    }
+
+    private void PopScope()
+    {
+        if (currentScope.Parent is null)
+        {
+            throw new InvalidOperationException();
+        }
+        currentScope = currentScope.Parent;
+    }
+
+    private void AddSymbol(Symbol symbol)
+    {
+        currentScope.AddSymbol(symbol);
+    }
+}
