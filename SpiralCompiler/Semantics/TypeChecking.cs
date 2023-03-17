92bdd2bb184ff90129bd75b15a699c9f38034977
lucyelle <lucyelle@users.noreply.github.com>
2023-03-17 21:12:51 +0100

    More type checking

--- SpiralCompiler/Semantics/TypeChecking.cs
+++ SpiralCompiler/Semantics/TypeChecking.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using SpiralCompiler.Syntax;
 
 namespace SpiralCompiler.Semantics;
@@ -14,9 +15,109 @@ public class TypeChecking : AstVisitorBase<Symbol.Type?>
         var symbol = (Symbol.ITyped)node.Symbol;
         if (symbol.SymbolType is null)
         {
-            throw new InvalidOperationException("type checker didnt fill out symbol type");
+            throw new InvalidOperationException("type checker didn't fill out symbol type");
         }
         return symbol.SymbolType;
+    }
+
+    protected override Symbol.Type? VisitVarStatement(Statement.Var node)
+    {
+        var symbol = (Symbol.ITyped?)node.Symbol ?? throw new InvalidOperationException("var statement sybol is null");
+
+        if (node.Type is null)
+        {
+            if (node.Value is null)
+            {
+                throw new InvalidOperationException("var type can't be null if value is not assigned");
+            }
+            var valueType = VisitExpression(node.Value);
+            symbol.SymbolType = valueType;
+        }
+        else
+        {
+            var varType = VisitTypeReference(node.Type) ?? throw new InvalidOperationException();
+            symbol.SymbolType = varType;
+
+            if (node.Value is not null)
+            {
+                var valueType = VisitExpression(node.Value) ?? throw new InvalidOperationException();
+                if (!Symbol.Type.IsAssignable(varType, valueType))
+                {
+                    throw new InvalidOperationException("var type and value type does not match");
+                }
+            }
+        }
+
+        return symbol.SymbolType;
+    }
+
+    protected override Symbol.Type? VisitBinaryExpression(Expression.Binary node)
+    {
+        var leftType = VisitExpression(node.Left);
+        var rightType = VisitExpression(node.Right);
+
+        if (node.Op is BinOp.Or or BinOp.And)
+        {
+            if (leftType != BuiltInTypes.Boolean || rightType != BuiltInTypes.Boolean)
+            {
+                throw new InvalidOperationException("operator mismatch");
+            }
+            return BuiltInTypes.Boolean;
+        }
+        else if (node.Op is BinOp.Add)
+        {
+            // TODO: string or numbers
+        }
+        else if (node.Op is BinOp.AddAssign)
+        {
+            // TODO: string or numbers
+        }
+        else if (node.Op is BinOp.Substract or BinOp.Multiply or BinOp.Divide)
+        {
+            // TODO: numbers
+        }
+        else if (node.Op is BinOp.Assign)
+        {
+            // TODO: assignable
+        }
+        else if (node.Op is BinOp.SubtractAssign or BinOp.MultiplyAssign or BinOp.DivideAssign)
+        {
+
+        }
+        else if (node.Op is BinOp.Less or BinOp.LessEquals or BinOp.Greater or BinOp.GreaterEquals)
+        {
+
+        }
+        else if (node.Op is BinOp.Equals or BinOp.NotEqual)
+        {
+
+        }
+    }
+
+    protected override Symbol.Type? VisitUnaryPostExpression(Expression.UnaryPost node)
+    {
+        var leftType = VisitExpression(node.Left) ?? throw new InvalidOperationException();
+        if (!Symbol.Type.IsNumeric(leftType))
+        {
+            throw new InvalidOperationException("operation mismatch with int/double expression");
+        }
+        return leftType;
+    }
+
+    protected override Symbol.Type? VisitUnaryPreExpression(Expression.UnaryPre node)
+    {
+        var rightType = VisitExpression(node.Right);
+
+        if (node.Op is UnOpPre.Not)
+        {
+            // TODO: bool
+        }
+        else
+        {
+            // TODO: numerics
+        }
+
+        return rightType;
     }
 
     protected override Symbol.Type? VisitFunctionCallExpression(Expression.FunctionCall node)
